05d7ef8d36c7114d952f730d1333006d7bbb8e49
Vynt <nguyentrieuvy97@ntrieuvy97gmail.com>
2023-05-08 13:43:11 +0700

    init-project

---
+++ src/components/atoms/Menu/SubMenu.component.tsx
@@ -0,0 +1,9 @@
+import { SubMenuProps } from 'antd'
+import AntSubMenu from 'antd/lib/menu/SubMenu'
+import React from 'react'
+
+export const SubMenu = (props: SubMenuProps) => {
+  return (
+    <AntSubMenu {...props} />
+  )
+}
